916b977f00a1bf52b21ca5cd57f8ee99ae193036
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-10-11 19:01:35 -0300

    cookie config

--- api/src/server.js
+++ api/src/server.js
@@ -3,11 +3,13 @@ require("express-async-errors");
 const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
-
+const cookieParser = require('cookie-parser')
 const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
+
 app.use(cors({
   origin: ["http://localhost:5173", "http://127.0.0.1::5173/"],
   credentials: true // utilizar o cookie
